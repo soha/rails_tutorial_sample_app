d0d7c18d77330cf5435de91c3b9808b69bbd4a19
sohatach <sohatach+git@gmail.com>
2014-03-31 00:50:25 +0900

    9.3まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update, :index]
+  before_filter :correct_user,   only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -37,4 +40,22 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
