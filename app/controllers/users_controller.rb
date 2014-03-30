24b63c248521308f279c263d34fdfd59123c346f
sohatach <sohatach+git@gmail.com>
2014-03-30 15:55:27 +0900

    9.1まで。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,19 @@ class UsersController < ApplicationController
     sign_out
     redirect_to root_url
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 end
